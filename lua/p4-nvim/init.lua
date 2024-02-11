7bd3f3df573313b0afbd86dd62f7e39dc8897124
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2024-02-11 21:39:03 +0100

    Added automatic chmod +w and buffer reload

--- lua/p4-nvim/init.lua
+++ lua/p4-nvim/init.lua
@@ -5,5 +5,6 @@ local M = {}
 M.checkout = p4.checkout
 M.add = p4.add
 M.changelists = p4.changelists
+M.setup = p4.setup
 
 return M
--- lua/p4-nvim/p4.lua
+++ lua/p4-nvim/p4.lua
@@ -56,6 +56,7 @@ function M.checkout()
     local bufname = vim.fn.bufname()
     local cmd = "p4 edit -c " .. tostring(cl_num) .. " " .. bufname
     debug_print(cmd)
+    vim.fn.system('chmod +w ' .. bufname)
     local job = vim.fn.system(cmd)
     vim.notify("\n" .. job)
 end
@@ -76,5 +77,21 @@ function M.add()
     vim.notify("\n" .. job)
 end
 
+function M.setup()
+    -- Triger `autoread` when files changes on disk
+    -- https://unix.stackexchange.com/questions/149209/refresh-changed-content-of-file-opened-in-vim/383044#383044
+    -- https://vi.stackexchange.com/questions/13692/prevent-focusgained-autocmd-running-in-command-line-editing-mode
+    vim.api.nvim_create_autocmd({'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI'}, {
+      pattern = '*',
+      command = "if mode() !~ '\v(c|r.?|!|t)' && getcmdwintype() == '' | checktime | endif",
+    })
+
+    -- Notification after file change
+    -- https://vi.stackexchange.com/questions/13091/autocmd-event-for-autoread
+    vim.api.nvim_create_autocmd({'FileChangedShellPost'}, {
+      pattern = '*',
+      command = "echohl WarningMsg | echo 'File changed on disk. Buffer reloaded.' | echohl None",
+    })
+end
 
 return M
